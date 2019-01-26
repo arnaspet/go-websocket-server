157ffb94a3c6a22a410c31c419176ef6a752a607
Arnas Petruškevičius <arnas.petruskevicius@yahoo.com>
2019-01-26 13:34:51 +0200

    Improve ReplaceBytes test with table tests

--- server/domain/text_replacer_test.go
+++ server/domain/text_replacer_test.go
@@ -1,14 +1,23 @@
 package domain
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestReplaceBytes(t *testing.T) {
-	assert.Equal(t, []byte("!"), ReplaceBytes([]byte("?")))
-	assert.Equal(t, []byte("!"), ReplaceBytes([]byte("!")))
-	assert.Equal(t, []byte("hello!"), ReplaceBytes([]byte("hello?")))
-	assert.Equal(t, []byte("!a!b!c!d!"), ReplaceBytes([]byte("?a?b?c?d?")))
+	cases := []struct{input, output []byte} {
+		{[]byte("?"), []byte("!")},
+		{[]byte("!"), []byte("!")},
+		{[]byte("hello?"), []byte("hello!")},
+		{[]byte("?a?b?c?d?"), []byte("!a!b!c!d!")},
+	}
+
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("%s replaces to %s", tc.input, tc.output), func(t *testing.T) {
+			assert.Equal(t, tc.output, ReplaceBytes(tc.input))
+		})
+	}
 }
